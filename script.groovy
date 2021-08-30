6a956d0477e13f2a417b9e7870ea07e4f60c9f9c
fdamoni <77584343+fdamoni@users.noreply.github.com>
2021-08-30 13:32:03 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -4,10 +4,19 @@ def buildApp() {
 
 def testApp() {
     
+    
+    def str = "Hello world"
+    def num =0
     def arg = ["Groovy","Java","Python"]
+    
+    for(x in 1..5){
+    println x //0,1,2,3,4,5
+
     println arg.class
     println arg[1]
     echo 'testing the application...'
+    def str = "Hello world"
+    def num =0
     
 } 
 
